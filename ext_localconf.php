68f8a4b91787fec0679261643b628b47fa7ade2e
Ghanshyam Bhava <ghanshyam@devzspace.com>
2026-04-28 17:38:06 +0000

    [FEATURE] Add support for TYPO3 14 LTS
    Adapt the extension to the API changes between TYPO3 13 and
    TYPO3 14 LTS, covering signature changes in the workspace stage
    service, updated icon registry usage and adjusted view rendering
    helpers. The extension continues to support TYPO3 13 LTS in
    parallel via runtime version checks.

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,10 +1,3 @@
 <?php
 
 defined('TYPO3') or die();
-
-(static function () {
-    // Register backend module route
-    $GLOBALS['TYPO3_CONF_VARS']['BE']['defaultUserTSconfig'] .= '
-        options.pageTree.doktypesToShowInNewPageDragArea := addToList(254)
-    ';
-})();

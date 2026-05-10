69b45657f0a48351748799b67612652ce711d113
GhanshyamBhava <ghanshyambhava@yahoo.com>
2026-05-10 18:24:29 +0000

    [TASK] Override reset workspace stage using `DataHandler hook`
    Using PSR-14 event `AfterDataGeneratedForWorkspaceEvent` to scan
    for records which has been reset to TYPO3 default stage together
    with configured custom default stage (extension setting) and set
    it to the custom stage is not the right solution. This happens
    only on read still keeping the records after safe in the wrong
    stage.
    
    This change drops the PSR-14 event listener and switch to a
    `DataHandler hook` now to do the same with additional safe
    guarding that selected stage from extension configuration
    really exists to avoid TYPO3 exception.

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,3 +1,6 @@
 <?php
 
 defined('TYPO3') or die();
+
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] =
+    \WebVision\KanbanWorkspaces\Hooks\DefaultStageDataHandlerHook::class;
